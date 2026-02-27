8064de22a18039ae282581f6e1990abd822f83d9
nurzhan <nurzhan.zhukesh@gmail.com>
2026-02-27 07:35:28 +0100

    finishing basic implementation from an arena tutorial

--- arena/main.c
+++ arena/main.c
@@ -1,8 +1,13 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -25,13 +30,15 @@ typedef i32 b32;
 #define ARENA_BASE_POS (sizeof(mem_arena))
 #define ARENA_ALIGN (sizeof(void*))
 
-typedef struct
-{
-    u64 capacity;
+typedef struct {
+    u64 reserve_size;
+    u64 commit_size;
+
     u64 pos;
+    u64 commit_pos;
 } mem_arena;
 
-mem_arena* arena_create(u64 capacity);
+mem_arena* arena_create(u64 reserve_size, u64 commit_size);
 void arena_destroy(mem_arena* arena);
 void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
 void arena_pop(mem_arena* arena, u64 size);
@@ -44,47 +51,73 @@ void arena_clear(mem_arena* arena);
 #define PUSH_ARRAY(arena, T, n) (T*)arena_push((arena), sizeof(T) * (n), false)
 #define PUSH_ARRAY_NZ(arena, T, n) (T*)arena_push((arena), sizeof(T) * (n), true)
 
+u32 plat_get_pagesize(void);
+
+void* plat_mem_reserve(u64 size);
+b32 plat_mem_commit(void* ptr, u64 size);
+b32 plat_mem_decommit(void* ptr, u64 size);
+b32 plat_mem_release(void* ptr, u64 size);
+
 
 int main(void) {
-    mem_arena* perm_arena = arena_create(KiB(1));
+    mem_arena* arena = arena_create(GiB(1), MiB(100));
 
-    i64* new_array = PUSH_ARRAY(perm_arena, i64, 100);
-
-    for (int i = 0; i < 100; i++) {
-        new_array[i] = i;
+    while(1) {
+        arena_push(arena, MiB(20), false);
+        getc(stdin);
     }
 
-    for (int i = 0; i < 100; i++) {
-        printf("Element %d, value %ld\n", i, new_array[i]);
-    }
-
-    printf("Availavle space in arena: %lu\n", arena_available(perm_arena));
-
-    arena_destroy(perm_arena);
+    arena_destroy(arena);
 
     return 0;
 }
 
 
-mem_arena* arena_create(u64 capacity) {
-    mem_arena* arena = (mem_arena*)malloc(capacity);
+mem_arena* arena_create(u64 reserve_size, u64 commit_size) {
+    u32 pagesize = plat_get_pagesize();
 
-    arena->capacity = capacity;
+    reserve_size = ALIGN_UP_POW2(reserve_size, pagesize);
+    commit_size = ALIGN_UP_POW2(commit_size, pagesize);
+
+    mem_arena* arena = plat_mem_reserve(reserve_size);
+
+    if (!plat_mem_commit(arena, commit_size)) {
+        return NULL;
+    }
+
+    arena->reserve_size = reserve_size;
+    arena->commit_size = commit_size;
     arena->pos = ARENA_BASE_POS;
+    arena->commit_pos = commit_size;
 
-    printf("New %lu byte arena was created.\n", capacity);
     return arena;
 }
 
 void arena_destroy(mem_arena* arena) {
-    free(arena);
+    plat_mem_release(arena, arena->reserve_size);
 }
 
 void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
     u64 pos_aligned = ALIGN_UP_POW2(arena->pos, ARENA_ALIGN);
     u64 new_pos = pos_aligned + size;
 
-    if (new_pos > arena->capacity) { return NULL; }
+    if (new_pos > arena->reserve_size) { return NULL; }
+
+    if (new_pos > arena->commit_pos) {
+        u64 new_commit_pos = new_pos;
+        new_commit_pos += arena->commit_size - 1;
+        new_commit_pos -= new_commit_pos % arena->commit_size;
+        new_commit_pos = MIN(new_commit_pos, arena->reserve_size);
+
+        u8* mem = (u8*)arena + arena->commit_pos;
+        u64 commit_size = new_commit_pos - arena->commit_pos;
+
+        if (!plat_mem_commit(mem, commit_size)) {
+            return NULL;
+        }
+
+        arena->commit_pos = new_commit_pos;
+    }
 
     arena->pos = new_pos;
 
@@ -92,19 +125,15 @@ void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
 
     if (!non_zero) {
         memset(out, 0, size);
-        printf("Newly pushed memory in arena was zeroed.\n");
     }
 
-    printf("Pushed %lu bytes onto the arena.\n", size);
     return out;
 }
 
 void arena_pop(mem_arena* arena, u64 size) {
     if (size > arena->pos - ARENA_BASE_POS) {
         size = arena->pos - ARENA_BASE_POS;
-        printf("Whole arena is cleared\n");
     }
-    printf("Popped %lu bytes.\n", size);
     arena->pos -= size;
 }
 
@@ -113,10 +142,37 @@ void arena_pop_to(mem_arena* arena, u64 new_pos) {
     arena_pop(arena, size);
 }
 
-u64 arena_available(mem_arena* arena) {
-    return arena->capacity - arena->pos;
-}
 
 void arena_clear(mem_arena* arena) {
     arena_pop_to(arena, ARENA_BASE_POS);
+}
+
+
+u32 plat_get_pagesize(void) {
+    return (u32)sysconf(_SC_PAGESIZE);
+}
+
+void* plat_mem_reserve(u64 size) {
+    void* out = mmap(NULL, size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (out == MAP_FAILED) {
+        return NULL;
+    }
+    return out;
+}
+
+b32 plat_mem_commit(void* ptr, u64 size) {
+    i32 ret = mprotect(ptr, size, PROT_READ | PROT_WRITE);
+    return ret == 0;
+}
+
+b32 plat_mem_decommit(void* ptr, u64 size) {
+    i32 ret = mprotect(ptr, size, PROT_NONE);
+    if (ret != 0) { return false; }
+    ret = madvise(ptr, size, MADV_DONTNEED);
+    return ret == 0;
+}
+
+b32 plat_mem_release(void* ptr, u64 size) {
+    i32 ret = munmap(ptr, size);
+    return ret == 0;
 }
